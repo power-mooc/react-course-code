7ffee7611af47dbb40671e70d8914e88940bdb40
power <1410837981@qq.com>
2024-08-20 23:34:07 +0800

    feat: controlled

---
+++ controlled-and-uncontrolled/src/App.tsx
@@ -0,0 +1,11 @@
+import { ChangeEvent } from 'react';
+
+function App() {
+  function onChange(event: ChangeEvent<HTMLInputElement>) {
+    console.log(event.target.value);
+  }
+
+  return <input defaultValue={'guang'} onChange={onChange} />;
+}
+
+export default App;
