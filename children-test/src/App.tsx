09a2956e8ff97ec6b9a4e87fd5c59b38a12d191e
power <1410837981@qq.com>
2024-12-08 13:12:57 +0800

    feat: Portal component

--- children-test/src/App.tsx
+++ children-test/src/App.tsx
@@ -1,12 +1,11 @@
-import React, { FC } from 'react';
-
+import React, { FC, useEffect, useRef } from 'react';
+import Portal from './Portal';
 interface AaaProps {
   children: React.ReactNode[];
 }
 
 const Aaa: FC<AaaProps> = (props) => {
   const { children } = props;
-  console.log(children);
   return (
     <div className="container">
       {children.map((item, index) => {
@@ -20,13 +19,33 @@ const Aaa: FC<AaaProps> = (props) => {
   );
 };
 
+// function App() {
+//   return (
+//     <Aaa>
+//       <a href="www.baidu.com">111</a>
+//       <a href="www.baidu.com">222</a>
+//       <a href="www.baidu.com">333</a>
+//     </Aaa>
+//   );
+// }
+
 function App() {
+  const containerRef = useRef<HTMLElement>(null);
+
+  const content = (
+    <div className="btn">
+      <button>按钮</button>
+    </div>
+  );
+
+  useEffect(() => {
+    console.log(containerRef);
+  }, []);
+
   return (
-    <Aaa>
-      <a href="www.baidu.com">111</a>
-      <a href="www.baidu.com">222</a>
-      <a href="www.baidu.com">333</a>
-    </Aaa>
+    <Portal attach={document.body} ref={containerRef}>
+      {content}
+    </Portal>
   );
 }
 
